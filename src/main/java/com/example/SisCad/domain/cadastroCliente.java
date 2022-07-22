96f71bd36d560f877f616ee12f9a5f6f56172a5d
emgomes3112 <emgomes@outlook.com.br>
2022-07-22 11:29:58 -0300

    teste

--- src/main/java/com/example/SisCad/domain/cadastroCliente.java
+++ src/main/java/com/example/SisCad/domain/cadastroCliente.java
@@ -2,13 +2,23 @@ package com.example.SisCad.domain;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class cadastroCliente {
+@Entity
+@Table(name = "Cliente")
+public class cadastroCliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final String nome;
     private final String dataDeNascimento;
